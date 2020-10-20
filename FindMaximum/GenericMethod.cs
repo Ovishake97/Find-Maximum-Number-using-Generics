4fc89ef87ed04f0dae17289071c13580819285a7
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-20 21:22:51 +0530

    [Abhishek] Add . Added to the UC4 branch

--- FindMaximum/GenericMethod.cs
+++ FindMaximum/GenericMethod.cs
@@ -6,44 +6,34 @@ namespace FindMaximum
 {
    public class GenericMethod<T> where T : IComparable
     {
-        /// With the help of CompareTo method
-        /// the entered numbers are checked for
-        /// condition and the maximum number out of them is returned.
-        /// In case two or more numbers are equal it throws exception
-        public T num1, num2, num3;
-        public GenericMethod(T num1, T num2, T num3)
+        /// We will accept an array of values 
+        /// and with the help of the parameterised constructor
+        /// it is initialised
+        public T[] values;
+        public GenericMethod(T[] values)
         {
-            this.num1 = num1;
-            this.num2 = num2;
-            this.num3 = num3;
+            this.values = values;
+        }
+        /// Sorting the array of the values
+        /// and returning it
+        public static T[] SortedValues(T[] values) {
+            Array.Sort(values);
+            return values;
         }
         /// Implementing a generic method to
         /// check for any data type
         /// and give the maximum out of them
-        public static T TestMaximum(T num1, T num2, T num3) {
+        /// by returning the first value out of the sorted array
+        public static T TestMaximum(T[] values) {
             Console.WriteLine("Maximum number is:");
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
-            {
-                return num1;
-            }
-            else if (num2.CompareTo(num3) > 0 && num2.CompareTo(num1) > 0)
-            {
-                return num2;
-            }
-            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
-            {
-                return num3;
-            }
-            else {
-                throw new Exception("Two or more of the entered numbers are same");
-            }
+            var sorted = SortedValues(values);
+            return sorted[^1];
         }
         /// Defining the TestMaximum method
         /// to internally call the static TestMaximum method
-        /// as asked in the refactored code
         public T TestMaximum()
         {
-            T max = GenericMethod<T>.TestMaximum(num1, num2, num3);
+            T max = GenericMethod<T>.TestMaximum(this.values);
             return max;
         }
     }
