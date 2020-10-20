068abe72bc8a835b9231ef78beb42c46bd9da1c6
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-20 19:03:40 +0530

    [Abhishek] Add . Added to the UC1 branch

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+    ///Invoking the methods defined  
+    /// to calculate the maximun number
+        static void Main(string[] args)
+        {
+            int result=GenericMethod.GetMaximumInt(20, 30, 50);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
